c6d860cc6b0f113db037cb2257b05d57cc54df9f
Chan Young Koh <c.young.koh@gmail.com>
2021-03-04 10:31:07 -0500

    weird. for some reason, pandas will recognize the entire data in csv as NaN

--- crawler.py
+++ crawler.py
@@ -3,10 +3,12 @@ import reddit_info
 import csv
 import time
 from datetime import datetime
-import csv_to_sqlite
+# import csv_to_sqlite
 from praw.models import MoreComments
 import mysql_conn
 import json
+import pandas as pd
+
 
 CLIENT_ID = reddit_info.Reddit.client_id
 CLIENT_SECRET = reddit_info.Reddit.secret
@@ -32,24 +34,24 @@ def scrape_reddit():
         submission_dict = vars(submission)
 
         # Title
-        print("[Title %03d] : " %(total_subreddit), submission.title)
+        print("[Title %03d] : " % (total_subreddit), submission.title)
         writelist = [datetime.utcfromtimestamp(submission.created_utc),
                      submission.title,
-                     submission_dict['selftext'].rstrip("\n"),
+                     submission_dict['selftext'].rstrip("\n\n\n"),
                      0]
 
         # Comment
         post = reddit.submission(id=submission_dict['id'])
         total_comment = 0
-
-
+        comments = []
         for top_level_comment in post.comments.list():
             if isinstance(top_level_comment, MoreComments):
                 continue
-            writelist.append(top_level_comment.body)
-                             #.body.strip("\n"))
+            # writelist.append(top_level_comment.body)
+            # .body.strip("\n"))
+            comments.append(top_level_comment.body.strip("\n"))
             total_comment += 1
-
+        writelist.append(comments)
         writelist[3] = total_comment
         scrape_data.append(writelist)
 
@@ -66,18 +68,23 @@ def write_csv(scrape_data, total_subreddit):
         for data in scrape_data:
             writer.writerow(data)
 
+
 def write_json(scrape_data, total_subreddit):
     with open(time.strftime("data_%Y%m%d.txt", time.localtime()), 'wt') as file:
         texts = []
         for _ in scrape_data:
             texts.append(json.loads(next(file).strip()))
 
+def make_dataframe():
+    data = pd.read_csv(r'data_20210304.csv')
+    df = pd.DataFrame(data, columns=['Date', 'Title', 'Body', 'Comments_Count' 'Comments'])
+    print(df)
 
 def crawler():
-    scrape_data, total_subreddit = scrape_reddit()
-    write_csv(scrape_data, total_subreddit)
-    #write_json(scrape_data, total_subreddit)
-
+    # scrape_data, total_subreddit = scrape_reddit()
+    # write_csv(scrape_data, total_subreddit)
+    make_dataframe()
+    # write_json(scrape_data, total_subreddit)
 
 
 # Press the green button in the gutter to run the script.
