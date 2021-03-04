33250e1d2d22aaf1f6d32143e3c253d355251996
Chan Young Koh <c.young.koh@gmail.com>
2021-03-03 23:52:29 -0500

    tokenization working (I can sleep T_T). Moving on to finding unique words and eliminating duplicates

--- tokenizer.py
+++ tokenizer.py
@@ -1,4 +1,19 @@
 import spacy
 
 nlp = spacy.load('en_core_web_sm')
-file = open("profanities.txt", "r")
+
+def tokenizer():
+    text_chunk = open('data_20210303.csv', encoding="utf8").read()
+    nlp_text = nlp(text_chunk)
+
+    for word in nlp_text:
+        print(word.text)
+
+    # with open(file, 'w', encoding='utf-8') as f:
+    #     for word in f:
+    #         sp(word)
+    #         print(word.text, word.pos_)
+    #
+
+if __name__ == "__main__":
+    tokenizer()
