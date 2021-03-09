2a1365c064f8a16838293b6a78f98c75f82be63b
Chan Young Koh <c.young.koh@gmail.com>
2021-03-09 14:16:19 -0500

    found the reason. comment_count and comment_body were for some reason attached to each other, causing datatype error when I tried to create the dataframe.

--- crawler.py
+++ crawler.py
@@ -9,6 +9,7 @@ import mysql_conn
 import json
 import pandas as pd
 
+pd.set_option("display.max_rows", None, "display.max_columns", None)
 
 CLIENT_ID = reddit_info.Reddit.client_id
 CLIENT_SECRET = reddit_info.Reddit.secret
@@ -26,8 +27,9 @@ def scrape_reddit():
     print("---------------------------------\n\n")
 
     scrape_data = []
+    headings = ["date", "title", "body", "comment_body"]
+    scrape_data.append(headings)
     total_subreddit = 0
-
     for submission in reddit.subreddit(SUB_REDDIT).hot(limit=LIMIT):
         submission.comments.replace_more
         total_subreddit += 1
@@ -37,12 +39,11 @@ def scrape_reddit():
         print("[Title %03d] : " % (total_subreddit), submission.title)
         writelist = [datetime.utcfromtimestamp(submission.created_utc),
                      submission.title,
-                     submission_dict['selftext'].rstrip("\n\n\n"),
-                     0]
+                     submission_dict['selftext'].rstrip("\n\n\n")]
 
         # Comment
         post = reddit.submission(id=submission_dict['id'])
-        total_comment = 0
+        # total_comment = 0
         comments = []
         for top_level_comment in post.comments.list():
             if isinstance(top_level_comment, MoreComments):
@@ -50,9 +51,9 @@ def scrape_reddit():
             # writelist.append(top_level_comment.body)
             # .body.strip("\n"))
             comments.append(top_level_comment.body.strip("\n"))
-            total_comment += 1
+            #total_comment += 1
         writelist.append(comments)
-        writelist[3] = total_comment
+        # writelist[3] = total_comment
         scrape_data.append(writelist)
 
         print("-----------------------------------------")
@@ -61,7 +62,7 @@ def scrape_reddit():
 
 
 def write_csv(scrape_data, total_subreddit):
-    with open(time.strftime("data_%Y%m%d.csv", time.localtime()),
+    with open(time.strftime("data.csv", time.localtime()),
               'wt', encoding='utf=8') as file:
         writer = csv.writer(file, delimiter=",", lineterminator="\n")
 
@@ -70,19 +71,23 @@ def write_csv(scrape_data, total_subreddit):
 
 
 def write_json(scrape_data, total_subreddit):
-    with open(time.strftime("data_%Y%m%d.txt", time.localtime()), 'wt') as file:
+    with open(time.strftime("data.txt", time.localtime()), 'wt') as file:
         texts = []
         for _ in scrape_data:
             texts.append(json.loads(next(file).strip()))
 
 def make_dataframe():
-    data = pd.read_csv(r'data_20210304.csv')
-    df = pd.DataFrame(data, columns=['Date', 'Title', 'Body', 'Comments_Count' 'Comments'])
+    data = pd.read_csv(r'data.csv')
+    # for _ in data:
+
+    df = pd.DataFrame(data, columns=['date', 'title', 'body', 'comment_body'])
+    for dtype in df.dtypes.iteritems():
+        print(dtype)
     print(df)
 
 def crawler():
-    # scrape_data, total_subreddit = scrape_reddit()
-    # write_csv(scrape_data, total_subreddit)
+    scrape_data, total_subreddit = scrape_reddit()
+    write_csv(scrape_data, total_subreddit)
     make_dataframe()
     # write_json(scrape_data, total_subreddit)
 
